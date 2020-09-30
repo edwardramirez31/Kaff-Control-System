1965ae5b8d7ad3bd24d65632827088910f4936e6
Edward Ramirez <edal_ramirez@hotmail.com>
2020-09-30 13:32:12 -0500

    Several Changes
    First of all, Registro class inherits from QTabWidget, so I don't need the QVBoxLayout() mainLayout anymore.
    
    The other change was grouping the code in the tab1Content function

--- registro.py
+++ registro.py
@@ -1,15 +1,27 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QFormLayout, QLineEdit, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QFormLayout, QLineEdit, QVBoxLayout, QPushButton, QTabWidget
 import sys
 
 
-class Registro(QWidget):
+class Registro(QTabWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Bolsa Kaff')
         # crea el objeto layout
-        self.mainLayout = QVBoxLayout()
+        # self.mainLayout = QVBoxLayout()
+        self.createTabs()
+        self.tab1Content()
+
+    def createTabs(self):
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.addTab(self.tab1, "Registro")
+        self.addTab(self.tab2, "Base de Datos")
+        self.tab1Layout = QVBoxLayout()
+        self.tab1.setLayout(self.tab1Layout)
+
+    def tab1Content(self):
+        # Client Data
         self.layoutOne = QFormLayout()
-        # manipula el layout
         self.layoutOne.addRow('Fecha', QLineEdit())
         self.layoutOne.addRow('NOMBRE', QLineEdit())
         self.layoutOne.addRow('CUMPLEAÑOS', QLineEdit())
@@ -17,26 +29,25 @@ class Registro(QWidget):
         self.layoutOne.addRow('DIRECCIÓN', QLineEdit())
         self.layoutOne.addRow('CIUDAD', QLineEdit())
         self.layoutOne.addRow('MÉTODO DE PAGO', QLineEdit())
-        self.mainLayout.addLayout(self.layoutOne)
-        # establece o lo agrega a la ventana donde deba ir
-        self.setLayout(self.mainLayout)
+        self.tab1Layout.addLayout(self.layoutOne)
+        # LABEL
         self.label = QLabel('PEDIDO', parent=self)
-        self.mainLayout.addWidget(self.label)
-
-        # Layout de las empanadas
+        self.tab1Layout.addWidget(self.label)
+        # Layout Cantidad de Empanadas
         self.layoutTwo = QFormLayout()
         self.layoutTwo.addRow('POLLO', QLineEdit())
         self.layoutTwo.addRow('CARNE', QLineEdit())
         self.layoutTwo.addRow('EMPANACHOS', QLineEdit())
         self.layoutTwo.addRow('CANTIDAD EMPANADAS', QLineEdit())
         self.layoutTwo.addRow('VALOR', QLineEdit())
-        self.mainLayout.addLayout(self.layoutTwo)
-        self.button = QPushButton('LIMPIAR')
-        self.button2 = QPushButton('GUARDAR')
+        self.tab1Layout.addLayout(self.layoutTwo)
+        # Buttons
+        self.button = QPushButton('GUARDAR')
+        self.button2 = QPushButton('LIMPIAR')
         self.button3 = QPushButton('ELIMINAR')
-        self.mainLayout.addWidget(self.button)
-        self.mainLayout.addWidget(self.button2)
-        self.mainLayout.addWidget(self.button3)
+        self.tab1Layout.addWidget(self.button)
+        self.tab1Layout.addWidget(self.button2)
+        self.tab1Layout.addWidget(self.button3)
 
 
 app = QApplication(sys.argv)
