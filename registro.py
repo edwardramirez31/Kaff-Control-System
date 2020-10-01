750fd174cbbbc8047c1dd150c11567e546be2749
Edward Ramirez <edal_ramirez@hotmail.com>
2020-09-30 21:23:20 -0500

    More Several Changes
    Now I changed the Tab1 Main Layout, this time, I had a QGridLayout and I also created some functions to create each
    widget iterating through a for loop, where each dictionary has a widget name as a key and its respective position
    in the grid as the value. Storing the widgets in another dictionary allows me to access them whenever I want with
    loops, so I can set, delete or get their text when I push  the button.
    
    Another big change was the calendar, this is another widget and I open it when the browse button emit the clicked signal
    When the calendar is clicked, I retrieve the date in a variable and put this in its Line Widget

--- registro.py
+++ registro.py
@@ -1,66 +1,152 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QFormLayout, QLineEdit, QVBoxLayout, QPushButton, QTabWidget
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QGridLayout, QLineEdit, QPushButton, QTabWidget, QComboBox, QCalendarWidget
 import sys
-from PyQt5.QtCore import *
+# from PyQt5.QtCore import *
+from datetime import date
+from sqlite3 import connect
 
 
 class Registro(QTabWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Bolsa Kaff')
-        # crea el objeto layout
-        # self.mainLayout = QVBoxLayout()
         self.createTabs()
-        self.tab1Content()
+        self.createLabels()
+        self.createLinesWidgets()
+        self.createButtons()
 
     def createTabs(self):
         self.tab1 = QWidget()
         self.tab2 = QWidget()
         self.addTab(self.tab1, "Registro")
         self.addTab(self.tab2, "Base de Datos")
-        self.tab1Layout = QVBoxLayout()
-        self.tab1.setLayout(self.tab1Layout)
 
-    def tab1Content(self):
+    def createLabels(self):
         # Client Data
-        self.layoutOne = QFormLayout()
-        self.layoutOne.addRow('Fecha', QLineEdit())
-        self.layoutOne.addRow('NOMBRE', QLineEdit())
-        self.layoutOne.addRow('CUMPLEAÑOS', QLineEdit())
-        self.layoutOne.addRow('CELULAR', QLineEdit())
-        self.layoutOne.addRow('DIRECCIÓN', QLineEdit())
-        self.layoutOne.addRow('CIUDAD', QLineEdit())
-        self.layoutOne.addRow('MÉTODO DE PAGO', QLineEdit())
-        self.tab1Layout.addLayout(self.layoutOne)
-        # LABEL
-        self.label = QLabel('PEDIDO', parent=self)
-        self.label.setStyleSheet(
-            "background: black; color: white;")
-        self.label.setAlignment(Qt.AlignCenter)
-        self.tab1Layout.addWidget(self.label)
-        # Layout Cantidad de Empanadas
-        self.layoutTwo = QFormLayout()
-        self.polloWidget = QLineEdit()
-        self.layoutTwo.addRow('POLLO', self.polloWidget)
-        self.layoutTwo.addRow('CARNE', QLineEdit())
-        self.layoutTwo.addRow('EMPANACHOS', QLineEdit())
-        self.layoutTwo.addRow('CANTIDAD EMPA.', QLineEdit())
-        self.layoutTwo.addRow('VALOR', QLineEdit())
-        self.tab1Layout.addLayout(self.layoutTwo)
-        # Buttons
-        self.button = QPushButton('GUARDAR')
-        self.button2 = QPushButton('LIMPIAR')
-        self.button2.clicked.connect(self.tab1Buttons)
+        self.grid = QGridLayout()
+        self.labelWidgets = {
+            "FECHA": (0, 0),
+            "NOMBRE": (1, 0),
+            "CUMPLEAÑOS": (2, 0),
+            "CELULAR": (3, 0),
+            "DIRECCIÓN": (4, 0),
+            "CIUDAD": (5, 0),
+            "MÉTODO DE PAGO": (6, 0),
+            "PEDIDO": (7, 0, 1, 3),
+            "POLLO": (8, 0),
+            "CARNE": (9, 0),
+            "EMPANACHOS": (10, 0),
+            "CANTIDAD DE EMPA": (11, 0),
+            "VALOR": (12, 0)
+        }
+        for labelName, position in self.labelWidgets.items():
+            if len(position) == 4:
+                self.label = QLabel(labelName)
+                self.label.setStyleSheet(
+                    "background-color: gray; color: gray")
+                self.grid.addWidget(
+                    self.label, position[0], position[1], position[2], position[3])
+            else:
+                self.label = QLabel(labelName)
+                self.grid.addWidget(
+                    self.label, position[0], position[1])
 
-        self.button3 = QPushButton('ELIMINAR')
-        self.tab1Layout.addWidget(self.button)
-        self.tab1Layout.addWidget(self.button2)
-        self.tab1Layout.addWidget(self.button3)
+    def createLinesWidgets(self):
 
-    def tab1Buttons(self):
-        self.polloWidget.setText("")
+        lineEditWidgets = {
+            "FECHA": (0, 1, 1, 2),
+            "NOMBRE": (1, 1, 1, 2),
+            "CUMPLEAÑOS": (2, 1),
+            "CELULAR": (3, 1, 1, 2),
+            "DIRECCIÓN": (4, 1, 1, 2),
+            "CIUDAD": (5, 1, 1, 2),
+            "POLLO": (8, 1, 1, 2),
+            "CARNE": (9, 1, 1, 2),
+            "EMPANACHOS": (10, 1, 1, 2),
+            "CANTIDAD DE EMPA": (11, 1, 1, 2),
+            "VALOR": (12, 1)
+
+        }
+        self.lineEditWidgets = {}
+        for widgetName, position in lineEditWidgets.items():
+            if len(position) == 2:
+                self.lineEdit = QLineEdit()
+                self.grid.addWidget(self.lineEdit, position[0], position[1])
+                self.lineEditWidgets[widgetName] = self.lineEdit
+            else:
+                self.lineEdit = QLineEdit()
+                self.grid.addWidget(
+                    self.lineEdit, position[0], position[1], position[2], position[3])
+                self.lineEditWidgets[widgetName] = self.lineEdit
+
+        # Getting the current date
+        today = str(date.today())
+        self.lineEditWidgets["FECHA"].setText(today)
+
+    def createButtons(self):
+        buttons = {
+            "Browse": (2, 2),
+            "CALCULATE": (12, 2),
+            "SAVE": (13, 0),
+            "CLEAR": (13, 1),
+            "DELETE": (13, 2)
+        }
+        self.buttons = {}
+        for widgetName, position in buttons.items():
+            self.button = QPushButton(widgetName)
+            self.grid.addWidget(self.button, position[0], position[1])
+            self.buttons[widgetName] = self.button
+        self.tab1.setLayout(self.grid)
+
+        # Buttons Signals
+        self.buttons["CLEAR"].clicked.connect(self.clearAll)
+        self.buttons["Browse"].clicked.connect(self.calendar)
+        self.buttons["CALCULATE"].clicked.connect(self.calculate)
+
+        # Creating the Combo Box Button
+        self.combo = QComboBox()
+        self.combo.addItems(['Efectivo', 'Nequi'])
+        self.grid.addWidget(self.combo, 6, 1, 1, 2)
+
+    def clearAll(self):
+        for widgetName, lineWidget in self.lineEditWidgets.items():
+            if widgetName == "FECHA":
+                pass
+            else:
+                lineWidget.setText("")
+
+    def calculate(self):
+        self.pollo = self.lineEditWidgets["POLLO"].text()
+        self.carne = self.lineEditWidgets["CARNE"].text()
+        self.empanachos = self.lineEditWidgets["EMPANACHOS"].text()
+        self.Total = int(self.pollo) + int(self.carne)+int(self.empanachos)
+        self.lineEditWidgets["CANTIDAD DE EMPA"].setText(str(self.Total))
+        if self.Total > 5:
+            self.value = 2300 * self.Total
+        else:
+            self.value = 2500 * self.Total
+        self.lineEditWidgets["VALOR"].setText(str(self.value))
+
+    def calendar(self):
+        self.cal = QCalendarWidget()
+        self.cal.setWindowTitle("Get Birthday")
+        self.cal.show()
+        self.cal.clicked.connect(self.dateB)
+
+    def dateB(self):
+        self.date = self.cal.selectedDate()
+        self.lineEditWidgets["CUMPLEAÑOS"].setText(
+            self.date.toString("yyyy-MM-dd"))
+
+    def save(self):
+        pass
 
 
-app = QApplication(sys.argv)
-window = Registro()
-window.show()
-sys.exit(app.exec_())
+def main():
+    app = QApplication(sys.argv)
+    window = Registro()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
